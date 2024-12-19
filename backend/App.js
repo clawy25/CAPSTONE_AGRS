66a693e215f34c8cc954ab68cbc95c2f0b4f0267
erramae <emdbugayong@gmail.com>
2024-12-20 02:09:23 +0800

    file rearrangment and back appjs update
    i hate mylife

--- backend/App.js
+++ backend/App.js
@@ -12,10 +12,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build'))); // Make sure 'frontend' is correct directory
+app.use(express.static(path.join(__dirname,'..', 'build'))); // Make sure 'frontend' is correct directory
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 });
 
 
