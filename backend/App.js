02d5e16c6679ff38a835d8d12050f817dc3adaef
erramae <emdbugayong@gmail.com>
2024-12-20 02:20:46 +0800

    file rearrange and backend appjs
    im going insane

--- backend/App.js
+++ backend/App.js
@@ -12,10 +12,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname,'..', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname,'build', 'index.html'))
 });
 
 
