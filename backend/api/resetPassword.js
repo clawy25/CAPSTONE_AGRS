b507229f78defc3ee4d12401233956611ccd0d71
erramae <emdbugayong@gmail.com>
2025-01-22 17:30:15 +0800

    update

--- backend/api/resetPassword.js
+++ backend/api/resetPassword.js
@@ -40,13 +40,13 @@ router.post('/reset-password', async (req, res) => {
     try {
       // Decode and validate the token to get user ID
 
-      const { data: user, error: tokenError } = await supabase.auth.getUser(token);
-      console.log("Decoded token:", user);
+      /* const { data: user, error: tokenError } = await supabase.auth.getUser(token);
+      console.log("Decoded token:", user); */
   
-      if (tokenError || !user) {
+      /* if (tokenError || !user) {
         console.error('Token validation error:', tokenError);
         return res.status(400).json({ error: 'Invalid or expired token.' });
-      }
+      } */
   
       // Update the user's password
       const { error: updateError } = await supabase.auth.updateUser({
